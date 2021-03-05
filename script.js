b0d61c41837f79a530c17370b74f512567a300ff
Ilya Tate <Itate265@west-mec.local>
2021-03-05 15:21:25 -0700

    Class 03/05/21

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ $(function() {
     let $information = $('#scrollto-information');
 
     // Fills bar to max-width when information scrollto btn is clicked
-    $information.on('click', function(){
+    function animateProgress() {
         $progressbar.width('0');
         $progressbar.animate({
             width: '100%'
         }, 2000);
+    }
+
+    window.onscroll = function(e) {
+        animateProgress()
+    }
+
+    $information.on('click', function () {
+        animateProgress();
     });
 });
